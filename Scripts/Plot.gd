4416b912de8fef73d94ed0014e073d1abf62a8e3
Dillon Matchett <dillon.matchett@gmail.com>
2024-04-13 16:42:59 -0300

    Pawn can read locations and move to them

--- Scripts/Plot.gd
+++ Scripts/Plot.gd
@@ -1,17 +1,20 @@
 # A place for attractions to go
 class_name Plot extends StaticBody2D
 
-# Plots Have An Attraction
-var attraction: Attraction
 # Cost To Unlock
 @export var unlock_cost: int
 # If it is unlocked
 @export var unlocked: bool = false
+# If this is built
+@export var built: bool = false
 
 # Has name through StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Plots Have An Attraction
+	var attraction: Attraction = $Attraction
+
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
