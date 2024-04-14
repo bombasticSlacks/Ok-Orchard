575ebde62505ca97cc86a7a41a6a88a0fadd123b
dereksgh <12520519+dereksgh@users.noreply.github.com>
2024-04-13 21:17:30 -0300

    #19 Added day/night ambience (#28)

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -9,6 +9,8 @@ var pawns: Array
 var timer
 var dayColor
 var clock
+var daytime_ambience
+var nighttime_ambience
 
 # Plots the game tracks
 @export var plots: Plot
@@ -22,6 +24,7 @@ var end_sunrise_hour = 8
 var start_sunset_hour = 18
 var end_sunset_hour = 22
 var max_day_color_alpha = 0.4
+var min_db = -40
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,6 +32,8 @@ func _ready():
 	timer = get_node("Timer")
 	dayColor = get_node("UI/DayLayer/DayColor")
 	clock = get_node("UI/Clock")
+	daytime_ambience = get_node("DaytimeAmbience")
+	nighttime_ambience = get_node("NighttimeAmbience")
 	timer.timeout.connect(_tick)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -47,12 +52,22 @@ func _format_time(hour, minute):
 	return "%d:%02d" % [hour, floor(minute/10)*10]
 
 func _set_day_color(current_hour, current_minute):
-	if current_hour >= start_sunrise_hour and current_hour <= end_sunrise_hour:
+	if current_hour >= start_sunrise_hour and (current_hour * 60 + current_minute) <= end_sunrise_hour * 60:
 		var sunrise_percent = float((current_hour*60 + current_minute) - (start_sunrise_hour*60)) / float(end_sunrise_hour*60 - start_sunrise_hour*60)
 		dayColor.color.a = (1-sunrise_percent) * max_day_color_alpha
-	elif current_hour >= start_sunset_hour and current_hour <= end_sunset_hour:
+	elif current_hour >= start_sunset_hour and (current_hour * 60 + current_minute) <= end_sunset_hour * 60:
 		var sunrise_percent = float((current_hour*60 +current_minute) - (start_sunset_hour*60)) / float(end_sunset_hour*60 - start_sunset_hour*60)
 		dayColor.color.a = sunrise_percent * max_day_color_alpha
+
+func _adjust_ambience(current_hour, current_minute):
+	if current_hour >= start_sunrise_hour and (current_hour * 60 + current_minute) <= end_sunrise_hour * 60:
+		var sunrise_percent = float((current_hour*60 + current_minute) - (start_sunrise_hour*60)) / float(end_sunrise_hour*60 - start_sunrise_hour*60)
+		daytime_ambience.set_volume_db((1-sunrise_percent) * min_db)
+		nighttime_ambience.set_volume_db((sunrise_percent) * min_db)
+	elif current_hour >= start_sunset_hour and (current_hour * 60 + current_minute) <= end_sunset_hour * 60:
+		var sunrise_percent = float((current_hour*60 +current_minute) - (start_sunset_hour*60)) / float(end_sunset_hour*60 - start_sunset_hour*60)
+		nighttime_ambience.set_volume_db((1-sunrise_percent) * min_db)
+		daytime_ambience.set_volume_db((sunrise_percent) * min_db)
 
 # Simplified Processing Only Runs When A Root Level Timer Finishes
 func _tick():
@@ -62,6 +77,7 @@ func _tick():
 	var current_hour = time[0]
 	var current_minute = time[1]
 	_set_day_color(current_hour, current_minute)
+	_adjust_ambience(current_hour, current_minute)
 
 	var formatted_time = _format_time(current_hour, current_minute)
 	clock.set_text(formatted_time)
