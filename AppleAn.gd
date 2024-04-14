9e539fd72d245c3dcb0032c3338afa373cda8e42
Katherine Hall <katherine.liz.hall@gmail.com>
2024-04-14 15:32:08 -0300

    Money pane

---
+++ AppleAn.gd
@@ -0,0 +1,13 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var anim : Animation= $AppleLoop.get_animation("AppleLoop")
+	#anim.loop_mode =(Animation.LOOP_LINEAR)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
