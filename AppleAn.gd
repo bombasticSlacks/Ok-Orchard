64e618ae2a1b5a891bce417d718965e1d3772d88
hallkliz <166949366+hallkliz@users.noreply.github.com>
2024-04-14 16:01:58 -0300

    Money UI (#44)
    * Color refs
    
    * Money pane
    
    * Update Splash.tscn
    
    ---------
    
    Co-authored-by: Katherine Hall <katherine.liz.hall@gmail.com>
    Co-authored-by: Dillon Matchett <dillon.matchett@gmail.com>

--- AppleAn.gd
+++ AppleAn.gd
@@ -1,9 +1,11 @@
-extends Node
+extends AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#var anim : Animation= $AppleLoop.get_animation("AppleLoop")
+	#anim.loop_mode =(Animation.LOOP_LINEAR)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
